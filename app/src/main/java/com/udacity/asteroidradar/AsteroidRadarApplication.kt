b1aef38d51956056d20b981eade9ead8edd8ffc0
MonaOmar <monaomar3720@gmail.com>
2023-02-17 14:52:39 +0200

    first commit

---
+++ app/src/main/java/com/udacity/asteroidradar/AsteroidRadarApplication.kt
@@ -0,0 +1,43 @@
+@file:Suppress("unused")
+
+package com.udacity.asteroidradar
+
+import android.app.Application
+import android.os.Build
+import androidx.work.*
+import com.udacity.asteroidradar.utils.Constants.DELETE_ASTEROIDS_WORK_NAME
+import com.udacity.asteroidradar.utils.Constants.REFRESH_ASTEROIDS_WORK_NAME
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
+
+class AsteroidRadarApplication : Application() {
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
+    override fun onCreate() {
+        super.onCreate()
+        delayedInit()
+    }
+
+    private fun delayedInit() {
+        applicationScope.launch {
+            setupRecurringWork()
+        }
+    }
+
+    private fun setupRecurringWork() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    setRequiresDeviceIdle(true)
+                }
+            }.build()
+
+
+    }
+}
